ebeff987b60eae766ae41453dd2ae7abcf76a904
Miha258 <rizoks29@gmail.com>
2023-09-09 02:27:42 +0300

    rload script

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-nohup python3 main.py &
+directory_path=$(dirname "$0")
+pids=$(pgrep -f "python3 bot/main.py")
+
+for pid in $pids; do
+    cmd=$(ps -o cmd -p $pid --no-headers)
+
+    if [[ "$cmd" == *"$directory_path"* ]]; then
+        kill $pid
+    fi
+done
