e9f2859f88a10cd186d295d03210b1efcf9c46aa
Miha258 <rizoks29@gmail.com>
2023-09-09 02:29:19 +0300

    Changes

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -3,10 +3,10 @@ import sys, os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-bot_token = "5170217792:AAEROxDDKYsGkfu9t4Ck0clU2UsVjbuO0YQ"
+bot_token = sys.argv[1]
 bot = Bot(token = bot_token)
-owner = "1095610815"
-bot_type = "inviting"
+owner = sys.argv[2]
+bot_type = sys.argv[3]
 storage = MemoryStorage()
 
 def get_channel():
