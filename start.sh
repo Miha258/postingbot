0bfe15084c6c23a30159349f1a108672a1bd1f87
Miha258 <rizoks29@gmail.com>
2023-09-10 02:49:03 +0300

    Changes

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ PIDS=$(ps aux | grep "$TARGET_PROCESS" | grep -v 'grep' | awk '{print $2}')
 if [ -z "$PIDS" ]; then
   echo "No processes found matching the criteria."
 else
+  MAIN_P=$(ps -o ppid= -p ${PIDS[0]})
+  kill "$MAIN_P"
   for PID in $PIDS; do
     echo "Killing process with PID: $PID"
     kill "$PID"
