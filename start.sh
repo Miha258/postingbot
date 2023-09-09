8cb3498c5512b755c75dbf8c534ce25111e054bd
Miha258 <rizoks29@gmail.com>
2023-09-10 02:51:06 +0300

    Changes

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ if [ -z "$PIDS" ]; then
   echo "No processes found matching the criteria."
 else
   MAIN_P=$(ps -o ppid= -p ${PIDS[0]})
+  echo $MAIN_P
   kill "$MAIN_P"
   for PID in $PIDS; do
     echo "Killing process with PID: $PID"
