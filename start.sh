a947125e5fe81eb982d7d9d63788fec3009167db
Miha258 <rizoks29@gmail.com>
2023-09-10 03:03:12 +0300

    Changes

--- start.sh
+++ start.sh
@@ -7,7 +7,10 @@ if [ -z "$PIDS" ]; then
   echo "No processes found matching the criteria."
 else
   MAIN_P=$(ps -o ppid= -p ${PIDS[0]})
-  echo $MAIN_P
+  first_element=$(echo "$MAIN_P" | head -n 1)
+  first_element_trimmed=$(echo "$first_element" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+  MAIN_P="$first_element_trimmed"
+
   kill "$MAIN_P"
   for PID in $PIDS; do
     echo "Killing process with PID: $PID"
@@ -17,3 +20,8 @@ else
 fi
 
 nohup python3 main.py &
+
+
+PIDS=$(ps aux | grep "$TARGET_PROCESS" | grep -v 'grep' | awk '{print $2}')
+MAIN_P=$(ps -o ppid= -p ${PIDS[0]})
+echo $MAIN_P
